25cc4e5e0384f66f6e505c22c85bd5d60ab2e39b
cherep721 <alexnorin721@gmail.com>
2020-03-16 21:50:10 +0300

    check homework

--- files.php
+++ files.php
@@ -6,8 +6,7 @@
  * @param string $filename - имя файла
  * @return string расширение файла
  */
-
-function getExtension($filename)
+function getExtension(string $filename): string
 {
     return substr(strrchr($filename, '.'), 1);
 }
@@ -17,23 +16,27 @@ function getExtension($filename)
  * @param string $folder - путь до папки
  * @return array вложенных массивов со всеми файлами
  */
-
-function readFolder($folder)
+function readFolder(string $folder): array
 {
+    $arrayFiles = [];
+
     $openFolder = opendir($folder);
     while ($fileOrFolder = readdir($openFolder)) {
-        if ($fileOrFolder == '.' || $fileOrFolder == '..' || !getExtension($fileOrFolder) == 'php') {
+        if ($fileOrFolder == '.' || $fileOrFolder == '..' || getExtension($fileOrFolder) !== 'php') {
             continue;
         }
+
         $temp = $folder . DIRECTORY_SEPARATOR . $fileOrFolder;
         if (is_dir($temp)) {
-            readFolder($temp);
+            $arrayFiles = array_merge($arrayFiles, readFolder($temp));
+            continue;
         }
 
         $arrayFiles[] = $fileOrFolder;
     }
-    return $arrayFiles;
     closedir($openFolder);
+
+    return $arrayFiles;
 }
 
 var_dump(readFolder($_SERVER['DOCUMENT_ROOT'] . '/templates'));
